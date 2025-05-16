ffa55bd61fe1fb993f1ee400d728a009a049c5bb
Ankur Sinha <ankur.sinha2019@gmail.com>
2025-05-16 13:47:47 +0530

    temp

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,10 @@ export default defineConfig({
   define: {
     global: 'globalThis',
   },
+  
   server: {
+    host: '0.0.0.0',
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',  // Proxy API requests
