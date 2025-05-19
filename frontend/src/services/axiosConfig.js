a44be1cf54115bae27372d58401ceaedaedae1db
Ankur Sinha <ankur.sinha2019@gmail.com>
2025-05-19 11:11:05 +0530

    temp

--- frontend/src/services/axiosConfig.js
+++ frontend/src/services/axiosConfig.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-const API_URL = import.meta.env.VITE_API_URL
+// const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL || "http://api-gateway-svc.auction-system.svc.cluster.local";
+
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
