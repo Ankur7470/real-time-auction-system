381f3578771778ef6fa89acb52d7bfcd92359239
Ankur Sinha <ankur.sinha2019@gmail.com>
2025-05-21 23:40:14 +0530

    Fix For Prod

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,36 +23,7 @@ export default defineConfig({
   // }
 })
 
-/* eslint-env node */
 
-// import { defineConfig, loadEnv } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig(({ mode }) => {
-//   // eslint-disable-next-line no-undef
-//   const env = loadEnv(mode, process.cwd(), '')
-
-//   return {
-//     plugins: [react()],
-//     define: {
-//       global: 'globalThis',
-//     },
-//     server: {
-//       proxy: {
-//         '/api': {
-//           target: env.VITE_API_BASE_URL, 
-//           changeOrigin: true,
-//         },
-//         '/ws-auction': {
-//           target: env.VITE_API_BASE_URL,
-//           ws: true,
-//           changeOrigin: true,
-//           secure: false,
-//         },
-//       },
-//     },
-//   }
-// })
 
 
 
