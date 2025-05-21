776963ff0c8cb1043482d38c7be544843136f2b3
Ankur Sinha <ankur.sinha2019@gmail.com>
2025-05-21 19:57:25 +0530

    Add Correct WebSocket

--- backend/api-gateway/src/main/java/com/auction_system/apigateway/JwtAuthenticationFilter.java
+++ backend/api-gateway/src/main/java/com/auction_system/apigateway/JwtAuthenticationFilter.java
@@ -29,7 +29,7 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
             "/api/auctions",
             "/api/bids/auction/**",
             "/api/auth/verify",
-           "/ws-auction",
+           "/ws-auction/**",
            "/actuator/**"
     );
 
