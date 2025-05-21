dc8b89ba6dd059953a60d1fb095be6f365a651bc
Ankur Sinha <ankur.sinha2019@gmail.com>
2025-05-21 19:22:29 +0530

    Fix Bug

--- backend/bidding-service/src/main/java/com/auction_system/biddingservice/config/WebSocketConfig.java
+++ backend/bidding-service/src/main/java/com/auction_system/biddingservice/config/WebSocketConfig.java
@@ -24,11 +24,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-auction")
-                .setAllowedOrigins(Arrays.asList(
-                    "http://localhost:5173",
-                    "http://192.168.49.2:31000", 
-                    "http://frontend-service.auction-system.svc.cluster.local"
-                ))
+                .setAllowedOriginPatterns("http://localhost:5173", "http://192.168.49.2:31000", "http://frontend-service.auction-system.svc.cluster.local")
+                // .setAllowedOrigins(Arrays.asList(
+                //     "http://localhost:5173",
+                //     "http://192.168.49.2:31000", 
+                //     "http://frontend-service.auction-system.svc.cluster.local"
+                // ))
                 .addInterceptors(authShakeInterceptor())
                 .setHandshakeHandler(new DefaultHandshakeHandler())
                 .withSockJS()
