779cedda5d01fa35ed6916a32b8fc97dfcd6afca
Ankur Sinha <ankur.sinha2019@gmail.com>
2025-05-21 16:03:55 +0530

    Fix Api Req

--- frontend/src/services/axiosConfig.js
+++ frontend/src/services/axiosConfig.js
@@ -1,8 +1,4 @@
 import axios from 'axios';
-// const API_URL = import.meta.env.VITE_API_URL
-//const API_URL = "http://192.168.49.2:31001";
-//const API_URL = "http://localhost:8000";
-
 
 const api = axios.create({
   baseURL: '/api',
